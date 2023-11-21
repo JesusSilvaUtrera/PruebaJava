60e2dc012737e90fcf96bc941137eed9d38fea92
JesusSilvaUtrera <jsilvautrera@gmail.com>
2023-11-21 21:08:29 +0100

    Included my first test program

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+      // This is a simple comment
+      /*
+      This is a block comment 
+      */
+      String text = "Hello world";
+      System.out.println("My text is : " + text);
+      System.out.print("Hello ");
+      System.out.print("this goes in the same line");
+      float num = 4.98f;
+      System.out.println(3 * 5 + 4 - 2);
+      System.out.println(num * 4);
+    }
+  }
